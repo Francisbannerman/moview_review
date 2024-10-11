e647ef7fafa7f2653bbba772440d0689903be7ec
Charles Agoe <agoetee@gmail.com>
2024-10-11 19:15:18 +0000

    #7 add permissions for views and jwt

--- accounts/views.py
+++ accounts/views.py
@@ -1,24 +1,57 @@
+from django.contrib.auth import authenticate, login
 from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from rest_framework.authtoken.models import Token
-from rest_framework import status
+from rest_framework import status, generics
 
 from .models import CustomUser
 from .serializers import CustomUserSerializer, LoginSerializer
 
 
-class UserRegistrationView(APIView):
-    def post(self, request):
-        serializer = CustomUserSerializer(data=request.data)
+class UserRegistrationView(generics.CreateAPIView):
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = [AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             user = serializer.save()
-            token, created = Token.objects.get_or_create(user=user)
-            return Response({"user": user.username, "key": token.key}, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(user.username, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_404_NOT_FOUND)
+
+# class UserRegistrationView(APIView):
+#     def post(self, request):
+#         serializer = CustomUserSerializer(data=request.data)
+#         if serializer.is_valid():
+#             user = serializer.save()
+#             token, created = Token.objects.get_or_create(user=user)
+#             return Response({"user": user.username, "key": token.key}, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class UserLoginView(APIView):
-    def post(self, request):
-        serializer = LoginSerializer(data=request.data)
-        if serializer.is_valid():
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = authenticate(email=serializer.validated_data["email"], password=serializer.validated_data["password"])
+        if user:
+            login(request, user)
+
+            return Response({"email":user.email, "message": "login successful"}, status=status.HTTP_200_OK)
+            
+        
+
+
+
+
+# class UserLoginView(APIView):
+#     def post(self, request):
+#         serializer = LoginSerializer(data=request.data)
+#         if serializer.is_valid():
+#             return Response(serializer.data, status=status.HTTP_200_OK)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
